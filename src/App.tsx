846a9dac24de041eff069bd07c174b6ecfb0f7db
Neil Rackett <neil.rackett@mesmotronic.com>
2025-05-31 06:17:08 +0100

    Made render optional

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,8 @@ const App: React.FC = () => {
     const cube = userData.cube!;
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    // Return false to prevent default render, e.g. if you're using an aleterative post-processing setup
   }, []);
 
   return (
